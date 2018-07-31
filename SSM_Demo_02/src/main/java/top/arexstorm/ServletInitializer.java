433e2554bb21ed2feab4eda2599e895c023c45e8
sandiegoe <sandiegoe@126.com>
2018-07-31 18:07:08 +0800

    SSM springboot init

---
+++ SSM_Demo_02/src/main/java/top/arexstorm/ServletInitializer.java
@@ -0,0 +1,13 @@
+package top.arexstorm;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SsmDemo02Application.class);
+	}
+
+}
---
+++ SSM_Demo_02/src/main/java/top/arexstorm/SsmDemo02Application.java
@@ -0,0 +1,12 @@
+package top.arexstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmDemo02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsmDemo02Application.class, args);
+	}
+}
