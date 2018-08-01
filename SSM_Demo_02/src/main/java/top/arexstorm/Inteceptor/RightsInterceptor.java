43e4ba8f84a50d9b9dd226122e1389b4c65758c0
sandiegoe <sandiegoe@126.com>
2018-08-01 12:32:35 +0800

    解决跨域访问报错

--- SSM_Demo_02/src/main/java/top/arexstorm/Inteceptor/RightsInterceptor.java
+++ SSM_Demo_02/src/main/java/top/arexstorm/Inteceptor/RightsInterceptor.java
@@ -12,25 +12,30 @@ public class RightsInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		System.out.println("start url " + request.getRequestURI() + " ...");
-		if (!(handler instanceof HandlerMethod)) { // 非handlerMethod 直接放行
-			System.out.println("ignore option url... " + request.getRequestURI());
-			System.out.println("ignore option method ... " + handler);
-			return true;
+		try {
+			System.out.println("start url " + request.getRequestURI() + " ...");
+			if (!(handler instanceof HandlerMethod)) { // 非handlerMethod 直接放行
+				System.out.println("ignore option url... " + request.getRequestURI());
+				System.out.println("ignore option method ... " + handler);
+				return true;
+			}
+			HandlerMethod method = (HandlerMethod) handler;
+			if (method.getMethodAnnotation(IgnorerRights.class) != null){ // 免登陆接口
+				System.out.println("do not need option url... " + request.getRequestURI());
+				return true;
+			}
+			String token = request.getParameter("token");
+			if (StringUtils.isNotBlank(token)) { // token is not blank
+				//对于token的进一步验证，或者 权限验证....
+				return true;
+				//throw Exception("No Access...");
+			}
+			throw new Exception("Go to login...");
+		} catch (Exception e) {
+			response.setHeader("Access-Control-Allow-Credentials", "true"); //解决跨域访问报错
+			response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin")); //解决跨域访问报错
+			throw e;
 		}
-		HandlerMethod method = (HandlerMethod) handler;
-		if (method.getMethodAnnotation(IgnorerRights.class) != null){ // 免登陆接口
-			System.out.println("do not need option url... " + request.getRequestURI());
-			return true;
-		}
-		String token = request.getParameter("token");
-		if (StringUtils.isBlank(token)) {
-			System.out.println("token is blank....");
-			return false;
-		}
-		//对于token的进一步验证，或者 权限验证....
-
-		return true;
 	}
 
 }
